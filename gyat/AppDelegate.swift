f90e9ff34c73b14a5833d9378c11f832e3aa0aca
Kentaro ku KUMAGAI <ku0522a@gmail.com>
2019-03-23 21:29:44 +0900

    Initial Commit

---
+++ gyat/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  gyat
+//
+//  Created by ku KUMAGAI Kentaro on 2019/03/23.
+//  Copyright © 2019 ku KUMAGAI Kentaro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
