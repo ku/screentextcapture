920ea189ba8b3d41dff1f40b8ae0129eee735ec0
ku KUMAGAI <ku0522a@gmail.com>
2020-06-07 21:51:45 +0900

    show email address

--- screentextcapture/AccessTokenStore.swift
+++ screentextcapture/AccessTokenStore.swift
@@ -17,6 +17,11 @@ private let kRedirectURI = "https://localhost/"
 class AccessTokenStore: NSObject {
     typealias AccessToken = String
 
+    enum AccountType: String {
+        case oAuth
+        case rawToken
+    }
+
     private var redirectHandler: RedirectHTTPHandler?
     private var state: OIDAuthState?
 
@@ -25,11 +30,27 @@ class AccessTokenStore: NSObject {
         case stateIsNil
         case invalidToken
         case invalidResponse
+        case networkError(Error)
+        case decodingError(String)
     }
 
     override init() {
         super.init()
         state = KeyStore().getAuthState()
+    }
+
+    var accountType: AccountType {
+        get {
+            guard let typeString = UserDefaults.standard.string(forKey: "AccountType"),
+                let type = AccountType(rawValue: typeString) else {
+                    return .oAuth
+            }
+            return type
+        }
+        set {
+            UserDefaults.standard.set(newValue.rawValue, forKey: "AccountType")
+            UserDefaults.standard.synchronize()
+        }
     }
 
     var hasValidToken: Bool {
@@ -58,7 +79,6 @@ class AccessTokenStore: NSObject {
     private func authorize(completion: @escaping (Result<AccessToken, Error>) -> Void) {
         let handler = RedirectHTTPHandler(successURL: URL(string: kSuccessURLString))
         let redirectUri = handler.startHTTPListener(nil)
-        print(redirectUri)
         self.redirectHandler = handler
 
         OIDAuthorizationService.discoverConfiguration(
@@ -83,6 +103,7 @@ class AccessTokenStore: NSObject {
             configuration: configuration,
             clientId: Environment.kGcpClientId, clientSecret: Environment.kGcpClientSecret,
             scopes: [
+                "https://www.googleapis.com/auth/userinfo.email",
                 "https://www.googleapis.com/auth/cloud-vision"
             ],
             redirectURL: redirectURI,
@@ -112,6 +133,54 @@ class AccessTokenStore: NSObject {
 
     private func save(state: OIDAuthState) {
         KeyStore().saveAuthState(state)
+        self.state = state
+    }
+
+    struct GoogleProfile: Decodable {
+        let sub: String
+        let picture: String
+        let email: String
+        let email_verified: Bool
+        let hd: String
+    }
+
+    func userInfo(completion: @escaping (Result<GoogleProfile, TokenError>) -> Void) {
+        guard let url = state?.lastAuthorizationResponse.request.configuration.discoveryDocument?.userinfoEndpoint else {
+            completion(.failure(error: .stateIsNil))
+            return
+        }
+
+        state?.performAction(freshTokens: { accessToken, idToken, error in
+            guard let accessToken = accessToken else { return }
+
+            var request = URLRequest(url: url)
+            request.addValue("Bearer \( accessToken )", forHTTPHeaderField: "Authorization")
+
+            let session = URLSession(configuration: URLSessionConfiguration.default)
+            let task: URLSessionDataTask = session.dataTask(with: request) { (data, response, error) in
+                DispatchQueue.main.async {
+                    if let error = error {
+                        completion(.failure(error: .networkError(error)))
+                        return
+                    }
+
+                    if let response = response as? HTTPURLResponse,
+                        response.statusCode == 200,
+                        let data = data {
+                        do {
+                            let profile = try JSONDecoder().decode(GoogleProfile.self, from: data)
+                            completion(.success(value: profile))
+                        } catch {
+                            let responseText = String(data: data, encoding: .utf8) ?? ""
+                            completion(.failure(error: .decodingError(responseText)))
+                        }
+                    } else {
+                        completion(.failure(error: TokenError.invalidResponse))
+                    }
+                }
+            }
+            task.resume()
+        })
     }
 }
 
--- screentextcapture/ViewController.swift
+++ screentextcapture/ViewController.swift
@@ -8,27 +8,34 @@
 
 import Cocoa
 
-
 class ViewController: NSViewController {
 
     @IBOutlet private var helpLabel: NSTextField!
     @IBOutlet private var accessTokenTextField: NSTextField!
     @IBOutlet private var notifyWhenDoneButton: NSButton!
+    @IBOutlet private var signInWithGoogleButton: NSButton!
 
-    private var tokenStore = AccessTokenStore()
+    @IBOutlet private var accountTypeOAuthButton: NSButton!
+    @IBOutlet private var accountTypeAccessTokenButton: NSButton!
+
+    private lazy var tokenStore: AccessTokenStore = {
+        (NSApplication.shared.delegate as! AppDelegate).accessTokenStore!
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         NotificationCenter.default.addObserver(self, selector: #selector(fieldTextDidChange), name: NSNotification.Name(rawValue: "NSTextDidChangeNotification"), object: nil)
 
-        tokenStore.perform(action: { result in
+        setupUI()
+
+        tokenStore.userInfo(completion: { [weak self] result in
             switch result {
-            case .success:
-                NSRunningApplication.current.activate(options: .init(arrayLiteral: .activateAllWindows, .activateIgnoringOtherApps))
+            case .success(let profile):
+                self?.signInWithGoogleButton.title = profile.email
             case .failure(let error):
-                // show alert
-                print(error)
+                self?.show(error: error
+                )
             }
         })
     }
@@ -54,8 +61,60 @@ class ViewController: NSViewController {
         Preferences().notifyWhenDone = (sender.state == .on)
     }
 
-    @IBAction private func didTapHelp(sender: Any) {
+    @IBAction private func didTapSignInWithGoogle(sender: Any) {
+        tokenStore.perform(action: { [weak self] result in
+            switch result {
+            case .success(let token):
+                print(token)
+            case .failure(let error):
+                self?.show(error: error)
+            }
+        })
+    }
 
-        //        NSWorkspace.shared.open(url)
+    @IBAction private func didTapHelp(sender: Any) {
+        guard let url = URL(string: "https://cloud.google.com/vision/docs/auth") else { return }
+        NSWorkspace.shared.open(url)
+    }
+
+    @IBAction private func radioButtonDidTap(sender: NSButton) {
+        if sender == accountTypeOAuthButton {
+            updateRadioButtonStatus(accountType: .oAuth)
+            tokenStore.accountType = .oAuth
+        } else if sender == accountTypeAccessTokenButton {
+            updateRadioButtonStatus(accountType: .rawToken)
+            tokenStore.accountType = .rawToken
+        }
+    }
+
+    private func setupUI() {
+        let image = NSImage(imageLiteralResourceName: "btn_google_light_normal_ios")
+        image.capInsets = .init(top: 4, left: 34, bottom: 4, right: 4)
+        signInWithGoogleButton.image = image
+        signInWithGoogleButton.imageScaling = .scaleAxesIndependently
+        updateRadioButtonStatus(accountType: tokenStore.accountType)
+    }
+
+    private func updateRadioButtonStatus(accountType: AccessTokenStore.AccountType) {
+        switch accountType {
+        case .oAuth:
+            accountTypeOAuthButton.state = .on
+            accountTypeAccessTokenButton.state = .off
+            accessTokenTextField.refusesFirstResponder = true
+            view.window?.makeFirstResponder(nil)
+        case .rawToken:
+            accountTypeOAuthButton.state = .off
+            accountTypeAccessTokenButton.state = .on
+            accessTokenTextField.refusesFirstResponder = false
+            accessTokenTextField.becomeFirstResponder()
+        }
+    }
+
+    private func show(error: Error) {
+        let alert = NSAlert.init()
+        alert.messageText = ""
+        alert.informativeText = error.localizedDescription
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
     }
 }
